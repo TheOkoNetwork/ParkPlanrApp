d53851efe0024cf307b5e4b7f46c6ca06205ff49
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-05-25 22:32:33 +0100

    deployment ID should now show

--- app/about/about-view-model.js
+++ app/about/about-view-model.js
@@ -1,6 +1,7 @@
 const observableModule = require('tns-core-modules/data/observable')
 const SelectedPageService = require('../shared/selected-page-service')
-var appversion = require('nativescript-appversion')
+const appversion = require('nativescript-appversion')
+const appSettings = require("tns-core-modules/application-settings");
 
 function AboutViewModel () {
   SelectedPageService.getInstance().updateSelectedPage('About')
@@ -19,6 +20,12 @@ function AboutViewModel () {
     viewModel.versionCode = versionCode
   })
 
+  appsyncCurrentPackageInfo = JSON.parse(appSettings.getString("APPSYNC_CURRENT_PACKAGE", "{}"));
+  console.log(AppsyncCurrentPackageInfo);
+  if (AppsyncCurrentPackageInfo.label) {
+    console.log(`Deployment version: ${AppsyncCurrentPackageInfo.label}`);
+    viewModel.DeploymentID = AppsyncCurrentPackageInfo.label;
+  }
   return viewModel
 }
 
