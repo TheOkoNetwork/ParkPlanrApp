997c2c6bb9a5d942561f4b660845aad6f7ae53e6
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-05-25 21:20:16 +0100

    more CI stuff added debug page

---
+++ app/debug/debug-page.js
@@ -0,0 +1,61 @@
+const app = require("tns-core-modules/application");
+const PageViewModel = require("./debug-view-model");
+const fromObject = require("tns-core-modules/data/observable").fromObject;
+
+const firebaseApp = require("nativescript-plugin-firebase/app");
+const firebase = require("nativescript-plugin-firebase");
+
+const appversion = require("nativescript-appversion");
+const appSettings = require("tns-core-modules/application-settings");
+
+
+function onNavigatingTo(args) {
+  const page = args.object;
+  page.bindingContext = new PageViewModel();
+
+
+  firebase.getCurrentPushToken().then((FCMToken) => {
+    console.log("Got FCMToken");
+    bindingContext = page.bindingContext._map;
+    bindingContext.FCMToken = FCMToken;
+
+    const vm = fromObject(bindingContext);
+          page.bindingContext = vm;
+  });
+
+  console.log("App settings");
+  appSettings.getAllKeys().forEach((key) => {
+    console.log(key);
+
+    value = "";
+    type = "*Type unknown*";
+
+    try {
+      value = appSettings.getBoolean(key);
+      type = "Boolean";
+    } catch (error) {
+    }
+    try {
+      value = appSettings.getNumber(key);
+      type = "Number";
+    } catch (error) {
+    }
+    try {
+      value = appSettings.getString(key);
+      type = "String";
+    } catch (error) {
+    }
+
+    console.log(type, value);
+  });
+}
+
+function onDrawerButtonTap(args) {
+    const sideDrawer = app.getRootView();
+    sideDrawer.showDrawer();
+}
+
+exports.onNavigatingTo = onNavigatingTo;
+exports.onDrawerButtonTap = onDrawerButtonTap;
+exports.pageJump = require("../shared/pageJump");
+exports.cmsPage = require("../shared/cmsPage");
---
+++ app/debug/debug-view-model.js
@@ -0,0 +1,64 @@
+const observableModule = require("tns-core-modules/data/observable");
+const SelectedPageService = require("../shared/selected-page-service");
+const appversion = require("nativescript-appversion");
+const platform = require("tns-core-modules/platform");
+const appSettings = require("tns-core-modules/application-settings");
+
+const AuthenticatedStateService = require("../shared/Authenticated-state-service");
+
+function PageViewModel() {
+  SelectedPageService.getInstance().updateSelectedPage("Debug");
+
+  const viewModel = observableModule.fromObject({
+    versionName: "",
+    versionCode: "",
+    DeploymentID: "APP",
+    user: false,
+    lastSubmittedTimestamp: "NONSET",
+    lastSubmittedType: "NONSET",
+    FCMToken: "Not registered",
+    AppID: "",
+    platform: ""
+  });
+
+  viewModel.platform = platform;
+
+  //viewModel.lastSubmittedTimestamp = JSON.parse(secureStorage.getSync({
+  //              key: "pushNotificationTokenLastSubmittedTimestamp"
+  //      }));
+  //      viewModel.lastSubmittedType = JSON.parse(secureStorage.getSync({
+  //              key: "pushNotificationTokenLastSubmittedType"
+  //      }));
+
+  AppsyncCurrentPackageInfo = JSON.parse(appSettings.getString("APPSYNC_CURRENT_PACKAGE", "{}"));
+        console.log(AppsyncCurrentPackageInfo);
+        if (AppsyncCurrentPackageInfo.label) {
+                console.log(`Deployment version: ${AppsyncCurrentPackageInfo.label}`);
+                viewModel.DeploymentID = AppsyncCurrentPackageInfo.label;
+        }
+
+  appversion.getVersionName().then((versionName) => {
+    console.log(`Your app's version is: ${versionName}`);
+    viewModel.versionName = versionName;
+  });
+  appversion.getVersionCode().then((versionCode) => {
+    console.log(`Your app's version code is: ${versionCode}`);
+    viewModel.versionCode = versionCode;
+  });
+  appversion.getVersionCode().then((versionCode) => {
+    console.log(`Your app's version code is: ${versionCode}`);
+    viewModel.versionCode = versionCode;
+  });
+  appversion.getAppId().then((AppID) => {
+    console.log(`Your app's app ID is: ${AppID}`);
+    viewModel.AppID = AppID;
+  });
+
+  AuthenticatedStateService.getInstance().AuthenticatedState$.subscribe((user) => {
+          viewModel.user = user;
+  });
+
+    return viewModel;
+}
+
+module.exports = PageViewModel;
