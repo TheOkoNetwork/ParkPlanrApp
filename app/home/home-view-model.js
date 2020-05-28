3525741c3965ce2d06cec263d0838cc0e52371f8
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-05-28 16:34:54 +0100

    home/home-view-model.js style fixes

--- app/home/home-view-model.js
+++ app/home/home-view-model.js
@@ -3,8 +3,8 @@ const observableModule = require('tns-core-modules/data/observable')
 const SelectedPageService = require('../shared/selected-page-service')
 const AuthenticatedStateService = require('../shared/Authenticated-state-service')
 
-const appSyncRun = require("../shared/appSyncRun");
-const application = require("tns-core-modules/application");
+const appSyncRun = require('../shared/appSyncRun')
+const application = require('tns-core-modules/application')
 
 function HomeViewModel () {
   SelectedPageService.getInstance().updateSelectedPage('Home')
@@ -20,22 +20,18 @@ function HomeViewModel () {
     viewModel.user = user
   })
 
-
-
   if (!global.appSyncInit) {
-   console.log("App sync has not been ran yet this session");
-   global.appSyncInit = true;
-   global.appSyncAndroidKey = global.DebugMode ? global.appSyncKeys.android.staging : global.appSyncKeys.android.production;
-   global.appSynciOSKey = global.DebugMode ? global.appSyncKeys.ios.staging : global.appSyncKeys.ios.production;
-   global.appSyncEnabled = true;
+    console.log('App sync has not been ran yet this session')
+    global.appSyncInit = true
+    global.appSyncAndroidKey = global.DebugMode ? global.appSyncKeys.android.staging : global.appSyncKeys.android.production
+    global.appSynciOSKey = global.DebugMode ? global.appSyncKeys.ios.staging : global.appSyncKeys.ios.production
+    global.appSyncEnabled = true
 
-   application.on(application.resumeEvent, () => {
-     appSyncRun();
-   });
-   appSyncRun();
+    application.on(application.resumeEvent, () => {
+      appSyncRun()
+    })
+    appSyncRun()
   }
-
-
 
   return viewModel
 }
