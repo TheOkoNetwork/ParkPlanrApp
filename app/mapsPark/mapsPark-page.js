430df29c03a25666542c350c9e2f921be1036b9f
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-05-29 02:02:02 +0100

    quite a few standardjs auto fixes

--- app/mapsPark/mapsPark-page.js
+++ app/mapsPark/mapsPark-page.js
@@ -1,67 +1,67 @@
-const app = require("tns-core-modules/application");
+const app = require('tns-core-modules/application')
 
-const MapsParkViewModel = require("./mapsPark-view-model");
-const fromObject = require("tns-core-modules/data/observable").fromObject;
-const frameModule = require("tns-core-modules/ui/frame");
+const MapsParkViewModel = require('./mapsPark-view-model')
+const fromObject = require('tns-core-modules/data/observable').fromObject
+const frameModule = require('tns-core-modules/ui/frame')
 
-const firebaseApp = require("nativescript-plugin-firebase/app");
-firebaseApp.initializeApp();
+const firebaseApp = require('nativescript-plugin-firebase/app')
+firebaseApp.initializeApp()
 
-var FeedbackPlugin = require("nativescript-feedback");
-var feedback = new FeedbackPlugin.Feedback();
-var color = require("color");
+var FeedbackPlugin = require('nativescript-feedback')
+var feedback = new FeedbackPlugin.Feedback()
+var color = require('color')
 
-function onNavigatingTo(args) {
-    const page = args.object;
-    page.bindingContext = new MapsParkViewModel();
+function onNavigatingTo (args) {
+  const page = args.object
+  page.bindingContext = new MapsParkViewModel()
 
-    var parkId = page.navigationContext.parkId;
-    console.log(`Loading map for park: ${parkId}`);
+  var parkId = page.navigationContext.parkId
+  console.log(`Loading map for park: ${parkId}`)
 
-    console.log("Fetching park data");
-    firebaseApp
-        .firestore()
-        .collection("parks")
-        .doc(parkId)
-        .get()
-        .then(function (parkSnapshot) {
-            console.log("park data");
-            var mapUrl = parkSnapshot.data().map;
-            const vm = fromObject({
-                mapUrl: mapUrl,
-                parkName: parkSnapshot.data().name,
-            });
-            page.bindingContext = vm;
+  console.log('Fetching park data')
+  firebaseApp
+    .firestore()
+    .collection('parks')
+    .doc(parkId)
+    .get()
+    .then(function (parkSnapshot) {
+      console.log('park data')
+      var mapUrl = parkSnapshot.data().map
+      const vm = fromObject({
+        mapUrl: mapUrl,
+        parkName: parkSnapshot.data().name
+      })
+      page.bindingContext = vm
+    })
+    .catch(function (error) {
+      console.log(`Error fetching map for park: ${parkId}`)
+      console.log(error)
+
+      frameModule.topmost().navigate({
+        moduleName: 'home/home-page',
+        transition: {
+          name: 'fade'
+        }
+      })
+      setTimeout(function () {
+        feedback.error({
+          title: 'Unable to load map',
+          message:
+                        'Please check your internet connection and try again',
+          titleColor: new color.Color('black')
         })
-        .catch(function (error) {
-            console.log(`Error fetching map for park: ${parkId}`);
-            console.log(error);
-
-            frameModule.topmost().navigate({
-                moduleName: "home/home-page",
-                transition: {
-                    name: "fade",
-                },
-            });
-            setTimeout(function () {
-                feedback.error({
-                    title: "Unable to load map",
-                    message:
-                        "Please check your internet connection and try again",
-                    titleColor: new color.Color("black"),
-                });
-            }, 125);
-        });
+      }, 125)
+    })
 }
 
-function onDrawerButtonTap(args) {
-    const sideDrawer = app.getRootView();
-    sideDrawer.showDrawer();
+function onDrawerButtonTap (args) {
+  const sideDrawer = app.getRootView()
+  sideDrawer.showDrawer()
 }
 
-exports.onNavigatingTo = onNavigatingTo;
-exports.onDrawerButtonTap = onDrawerButtonTap;
-exports.pageJump = require("../shared/pageJump");
-var AuthenticatedPageState = require("../shared/AuthenticatedPageState");
-exports.cmsPage = require("../shared/cmsPage");
-exports.AuthenticatedPageState = AuthenticatedPageState;
+exports.onNavigatingTo = onNavigatingTo
+exports.onDrawerButtonTap = onDrawerButtonTap
+exports.pageJump = require('../shared/pageJump')
+var AuthenticatedPageState = require('../shared/AuthenticatedPageState')
+exports.cmsPage = require('../shared/cmsPage')
+exports.AuthenticatedPageState = AuthenticatedPageState
--- app/signout/signout-page.js
+++ app/signout/signout-page.js
@@ -1,40 +1,40 @@
-const app = require("tns-core-modules/application");
+const app = require('tns-core-modules/application')
 
-const SignoutViewModel = require("./signout-view-model");
+const SignoutViewModel = require('./signout-view-model')
 
-const firebase = require("nativescript-plugin-firebase");
-const frameModule = require("tns-core-modules/ui/frame");
+const firebase = require('nativescript-plugin-firebase')
+const frameModule = require('tns-core-modules/ui/frame')
 
-function onNavigatingTo(args) {
-    const page = args.object;
-    page.bindingContext = new SignoutViewModel();
+function onNavigatingTo (args) {
+  const page = args.object
+  page.bindingContext = new SignoutViewModel()
 }
 
-function onDrawerButtonTap(args) {
-    const sideDrawer = app.getRootView();
-    sideDrawer.showDrawer();
+function onDrawerButtonTap (args) {
+  const sideDrawer = app.getRootView()
+  sideDrawer.showDrawer()
 }
-function SignOut(args) {
-    console.log("Sign out called");
-    firebase.logout();
+function SignOut (args) {
+  console.log('Sign out called')
+  firebase.logout()
 
+  setTimeout(function () {
+    AuthenticatedPageState()
     setTimeout(function () {
-        AuthenticatedPageState();
-        setTimeout(function () {
-            frameModule.topmost().navigate({
-                moduleName: "home/home-page",
-                transition: {
-                    name: "fade",
-                },
-            });
-        });
-    }, 250);
+      frameModule.topmost().navigate({
+        moduleName: 'home/home-page',
+        transition: {
+          name: 'fade'
+        }
+      })
+    })
+  }, 250)
 }
 
-exports.onNavigatingTo = onNavigatingTo;
-exports.onDrawerButtonTap = onDrawerButtonTap;
-exports.SignOut = SignOut;
-exports.pageJump = require("../shared/pageJump");
-var AuthenticatedPageState = require("../shared/AuthenticatedPageState");
-exports.cmsPage = require("../shared/cmsPage");
-exports.AuthenticatedPageState = AuthenticatedPageState;
+exports.onNavigatingTo = onNavigatingTo
+exports.onDrawerButtonTap = onDrawerButtonTap
+exports.SignOut = SignOut
+exports.pageJump = require('../shared/pageJump')
+var AuthenticatedPageState = require('../shared/AuthenticatedPageState')
+exports.cmsPage = require('../shared/cmsPage')
+exports.AuthenticatedPageState = AuthenticatedPageState
