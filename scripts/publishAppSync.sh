bca3faac6d10ba54f46794aeec7bbebf1ad6d781
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-05-31 17:30:09 +0100

    CI flow updates

--- scripts/publishAppSync.sh
+++ scripts/publishAppSync.sh
@@ -3,4 +3,4 @@
 dirname=$(dirname $0)
 version="$(node $dirname/get-version.js)"
 echo "Current app version: ${version}"
-~/npm-global/bin/nativescript-app-sync release "ParkPlanr Android" android --targetBinaryVersion "$version" --mandatory
+nativescript-app-sync release "ParkPlanr Android" android --targetBinaryVersion "$version" --mandatory
