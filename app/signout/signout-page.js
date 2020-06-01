35fbf26b7447cccd0fbf61079b40197c7c801f90
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-06-01 13:49:35 +0100

    Trying to fix sign out

--- app/signout/signout-page.js
+++ app/signout/signout-page.js
@@ -4,9 +4,14 @@ const SignoutViewModel = require('./signout-view-model')
 
 const firebaseApp = require('nativescript-plugin-firebase/app')
 firebaseApp.initializeApp()
+const firebase = require('nativescript-plugin-firebase')
 
 const frameModule = require('tns-core-modules/ui/frame')
 const config = require('../shared/config')
+
+var FeedbackPlugin = require('nativescript-feedback')
+var feedback = new FeedbackPlugin.Feedback()
+var color = require('color')
 
 function onNavigatingTo (args) {
   const page = args.object
@@ -21,23 +26,39 @@ function onDrawerButtonTap (args) {
 }
 async function SignOut (args) {
   console.log('Sign out called')
+  firebase.logout()
   firebaseApp
     .auth()
     .signOut()
     .then(function () {
-      console.log('Logout OK')
+      console.log('firebaseApp signOut')
 
-      AuthenticatedPageState()
+      firebase
+        .reloadUser()
+        .then(function () {
+          AuthenticatedPageState()
 
-      setTimeout(function () {
-        frameModule.topmost().navigate({
-          moduleName: 'home/home-page',
-          transition: {
-            name: 'fade'
+          setTimeout(function () {
+            frameModule.topmost().navigate({
+              moduleName: 'home/home-page',
+              transition: {
+                name: 'fade'
+              }
+            })
+          }, 500)
+        })
+        .catch(function (error) {
+          if (error) {
+            console.log('Error reloading user after signing out')
+            console.log(error)
+            feedback.error({
+              title: JSON.stringify(error),
+              titleColor: new color.Color('black')
+            })
           }
         })
-      }, 500)
     })
+
     .catch(function (error) {
       if (error) {
         console.log('Error signing out')
